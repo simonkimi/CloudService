50ac1382dfb0c81033b9b7999f7972eba02635dc
simonkimi <805757448@qq.com>
2021-03-11 23:24:41 +0800

    linux适配

--- apps/build_equipment/models.py
+++ apps/build_equipment/models.py
@@ -5,7 +5,7 @@ from user.models import User
 
 class BuildEquipmentModel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    cid = models.IntegerField(help_text='装备cid')
+    cid = models.IntegerField(default=0, help_text='装备cid')
     create_time = models.IntegerField(default=time)
 
     class Meta:
--- test/test_base.py
+++ test/test_base.py
@@ -1,15 +1,34 @@
+import base64
+import datetime
+import hashlib
+import hmac
+
 import requests
-import zlib
 
-url = 'http://login.jr.moefantasy.com/index/hmLogin/9a92f07a6f337787bdf3e0f3945a4b57&t=1615472895875&e=954337e21bb893b4fb3023425349bac9&gz=1&market=2&channel=100011&version=5.2.0'
-
-headers = {
-    'User-Agent': 'Dalvik/2.1.0 (Linux; U; Android 8.1.0; 16th Build/OPM1.171019.026)'
-}
-
-rsp = requests.post(url=url, headers=headers)
+from game.constant import PASS_KEY, PASS_KEY_HEAD
 
 
-print(zlib.decompress(rsp.content).decode())
-print(rsp.headers)
+def _encryption(url):
+    times = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
+    data = f"POST\n{times}\n/{url.split('/', 3)[-1]}"
+    mac = hmac.new(PASS_KEY.encode(), data.encode(), hashlib.sha1)
+    data = mac.digest()
+    return base64.b64encode(data).decode('utf-8'), times
 
+
+def _build_headers(url):
+    data, times = _encryption(url)
+    return {
+        'Accept-Encoding': 'gzip',
+        'User-Agent': 'okhttp/3.4.1',
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization': f'HMS {PASS_KEY_HEAD}:{data}',
+        'Date': times
+    }
+
+
+rsp = requests.post(url='https://passportapi.moefantasy.com/1.0/get/login/@self',
+                    data='{"platform":"0","appid":"0","app_server_type":"0","password":"huanghuan789","username":"hyl7806331"}',
+                    headers=_build_headers('https://passportapi.moefantasy.com/1.0/get/login/@self'))
+
+print(rsp.text)
