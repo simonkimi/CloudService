5738332d5a1e001e48a7cdb565e6815f17ad39d8
simonkimi <805757448@qq.com>
2021-03-15 12:35:59 +0800

    修复多扣点数问题

--- test/test_explore.py
+++ test/test_explore.py
@@ -3,14 +3,16 @@ import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE',
                       'cloudService.settings')
 import django
-
+from django.db.models import Q
 
 django.setup()
 
-from user.models import User
-from game.main import ExploreMain
+from user.models import UserProfile
 
-user = User.objects.get(username="simon_xu")
+user_profile = UserProfile.objects.all().filter(
+    (~Q(campaign_map=0) | ~Q(pvp_fleet=0) | Q(build_switch=True) | Q(equipment_switch=True))
+    & Q(point__gt=0)
+    & Q(switch=True))
 
-explore = ExploreMain(user)
-explore.main()
+for i in user_profile:
+    print(i.campaign_map != 0 or i.pvp_fleet != 0 or i.build_switch or i.equipment_switch)
